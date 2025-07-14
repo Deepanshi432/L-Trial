872ef7b916ecc8a76c9212c57e4b6465440e27fd
Liannaka Dadi <liannaka.dadi10@gmail.com>
2025-07-14 19:22:11 +0530

    add a comment line

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+// this is a commented line to check github repo
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
